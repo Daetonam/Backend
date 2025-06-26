32d12af8c74966f6b1ba3cec90f210d2bbf03abd
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-27 02:10:11 +0900

    init: initialize project setup

---
+++ src/main/groovy/daetonam/konnect/KonnectApplication.groovy
@@ -0,0 +1,13 @@
+package daetonam.konnect
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KonnectApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KonnectApplication, args)
+    }
+
+}
